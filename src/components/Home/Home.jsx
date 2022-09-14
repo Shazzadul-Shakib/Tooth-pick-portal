b8ab2488dafafb4458bea22719bc6ca3a55b1a68
Shazzadul-Shakib <shakib1186@gmail.com>
2022-09-14 22:13:11 +0600

    Responsive hero section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import chair from '../../assets/images/chair.png';
+import bg_hero from '../../assets/images/bg.png';
 const Home = () => {
   return (
     <div>
-      <h1>This is home</h1>
+      <div
+        style={{background: `url('${bg_hero}')`}}
+        className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} className=" max-w-auto lg:max-w-xl rounded-lg shadow-2xl" alt='Chair'/>
+          <div>
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+            <button className="btn btn-success text-white">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
