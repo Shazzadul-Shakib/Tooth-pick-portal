d7d29951242d862ef5ad8d05d8d97bd89659db59
Shazzadul-Shakib <shakib1186@gmail.com>
2022-09-15 11:17:47 +0600

    responsive treatment section in services added

--- src/components/Home/Services.jsx
+++ src/components/Home/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import fluoride from '../../assets/images/fluoride.png';
 import cavity from '../../assets/images/cavity.png';
 import whitening from '../../assets/images/whitening.png';
+import treatment from '../../assets/images/treatment.png';
 const Services = () => {
   return (
     <div className=' my-32'>
@@ -44,6 +45,19 @@ const Services = () => {
             </div>
           </div>
         </div>
+      </div>
+      {/* Another card */}
+      <div className='  flex justify-center md:flex-col '>
+      <div className="hero  min-h-full md:min-h-screen">
+        <div className="hero-content flex-col md:flex-row">
+          <img src={treatment} className=" max-w-auto md:max-w-sm rounded-lg shadow-2xl lg:mr-10" alt='Chair'/>
+          <div>
+            <h1 className="text-5xl font-bold">Exceptional Dental Care, <br/> on Your Terms</h1>
+            <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+            <button className="btn btn-success text-white">Get Started</button>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   );
